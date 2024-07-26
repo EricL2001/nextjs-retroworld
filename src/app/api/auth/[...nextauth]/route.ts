5f11fe37a827315e4c360574e18ad8b6e76bdde0
Eric Levasseur <eric.levass@gmail.com>
2024-07-26 12:14:25 -0400

    updated route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -28,8 +28,7 @@ export const authOptions: NextAuthOptions = {
     },
 };
 
-// const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST }
+export { handler as GET, handler as POST }
 
-export default NextAuth(authOptions);
